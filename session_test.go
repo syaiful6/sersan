b3b1ead2c9b7162364e84608ce856b56b1653d70
Syaiful Bahri <sbahri@indo-soft.com>
2019-01-22 17:50:22 +0700

    add more tests and separate storage tests

--- session_test.go
+++ session_test.go
@@ -1,22 +1,11 @@
 package sersan
 
 import (
-	"bytes"
 	"errors"
-	"net/http"
-	"net/http/httptest"
 	"reflect"
 	"testing"
 	"time"
 )
-
-// NewRecorder returns an initialized ResponseRecorder.
-func NewRecorder() *httptest.ResponseRecorder {
-	return &httptest.ResponseRecorder{
-		HeaderMap: make(http.Header),
-		Body:      new(bytes.Buffer),
-	}
-}
 
 var tntError = errors.New("Tnt Storage method used")
 
@@ -28,87 +17,13 @@ func (s *tntStorage) DestroyAllOfAuthId(authId string) error { return tntError }
 func (s *tntStorage) Insert(sess *Session) error             { return tntError }
 func (s *tntStorage) Replace(sess *Session) error            { return tntError }
 
-type mockOperation struct {
-	tag, id, authId string
-	session         *Session
-}
-
-type mockStorage struct {
-	sessions map[string]*Session
-	op       []*mockOperation
-}
-
-func (s *mockStorage) Get(id string) (*Session, error) {
-	s.op = append(s.op, &mockOperation{tag: "Get", id: id})
-	if v, ok := s.sessions[id]; ok {
-		return v, nil
-	}
-
-	return nil, nil
-}
-
-func (s *mockStorage) Destroy(id string) error {
-	s.op = append(s.op, &mockOperation{tag: "Destroy", id: id})
-	if _, ok := s.sessions[id]; ok {
-		delete(s.sessions, id)
-		return nil
-	}
-
-	return nil
-}
-
-func (s *mockStorage) DestroyAllOfAuthId(authId string) error {
-	nmap := make(map[string]*Session)
-	for k, ses := range s.sessions {
-		if ses.AuthID != authId {
-			nmap[k] = ses
-		}
-	}
-	s.sessions = nmap
-	s.op = append(s.op, &mockOperation{tag: "DestroyAllOfAuthId", authId: authId})
-
-	return nil
-}
-
-func (s *mockStorage) Insert(sess *Session) error {
-	s.op = append(s.op, &mockOperation{tag: "Insert", session: sess})
-	if old, ok := s.sessions[sess.ID]; ok {
-		return &SessionAlreadyExists{OldSession: old, NewSession: sess}
-	}
-
-	s.sessions[sess.ID] = sess
-	return nil
-}
-
-func (s *mockStorage) Replace(sess *Session) error {
-	s.op = append(s.op, &mockOperation{tag: "Replace", session: sess})
-	if _, ok := s.sessions[sess.ID]; ok {
-		s.sessions[sess.ID] = sess
-		return nil
-	}
-
-	return &SessionDoesNotExist{Session: sess}
-}
-
-func prepareMockStorage(sessions []*Session) *mockStorage {
-	sess := make(map[string]*Session)
-	for _, s := range sessions {
-		sess[s.ID] = s
-	}
-
-	return &mockStorage{
-		sessions: sess,
-		op:       []*mockOperation{},
-	}
-}
-
 func TestLoadSession(t *testing.T) {
 	tests := []struct {
 		storage Storage
 		cookie  string
 	}{
 		{&tntStorage{}, ""},
-		{prepareMockStorage([]*Session{}), "123456789-123456789-123456789-12"},
+		{NewStorageRecorder(), "123456789-123456789-123456789-12"},
 	}
 
 	for i, test := range tests {
@@ -132,7 +47,7 @@ func TestLoadSession(t *testing.T) {
 func TestLoadSessionExists(t *testing.T) {
 	sess := NewSession("123456789-123456789-123456789-12", "auth-id", time.Now().UTC())
 	sess.Values["foo"] = "bar"
-	storage := prepareMockStorage([]*Session{
+	storage := PrepareStorageRecorder([]*Session{
 		sess,
 	})
 	ss := NewServerSessionState(storage)
@@ -200,7 +115,7 @@ func TestNextExpires(t *testing.T) {
 }
 
 func TestSaveSessionNothing(t *testing.T) {
-	storage := prepareMockStorage([]*Session{})
+	storage := NewStorageRecorder()
 	ss := NewServerSessionState(storage)
 	token := &SaveSessionToken{now: time.Now().UTC(), sess: nil}
 	sess, err := ss.Save(token, make(map[interface{}]interface{}))
@@ -212,13 +127,14 @@ func TestSaveSessionNothing(t *testing.T) {
 		t.Errorf("Expected non nil err, returned %v", err)
 	}
 
-	if !reflect.DeepEqual(storage.op, []*mockOperation{}) {
-		t.Errorf("expected storage operation to be empty, returned %d instead", len(storage.op))
+	operations := storage.GetOperations()
+	if !reflect.DeepEqual(operations, []*RecorderOperation{}) {
+		t.Errorf("expected storage operation to be empty, returned %d instead", len(operations))
 	}
 }
 
 func TestSaveSessionInitialize(t *testing.T) {
-	storage := prepareMockStorage([]*Session{})
+	storage := NewStorageRecorder()
 	ss := NewServerSessionState(storage)
 	token := &SaveSessionToken{now: time.Now().UTC(), sess: nil}
 	data := map[interface{}]interface{}{
@@ -232,17 +148,17 @@ func TestSaveSessionInitialize(t *testing.T) {
 		t.Errorf("Expected non nil err, returned %v", err)
 	}
 
-	expectedOp := []*mockOperation{
-		&mockOperation{
-			tag:     "Insert",
-			session: sess,
+	expectedOp := []*RecorderOperation{
+		&RecorderOperation{
+			Tag:     "Insert",
+			Session: sess,
 		},
 	}
 	if !reflect.DeepEqual(sess.Values, data) {
 		t.Error("expected sess.Values to be equals written data")
 	}
 
-	if !reflect.DeepEqual(storage.op, expectedOp) {
+	if !reflect.DeepEqual(storage.GetOperations(), expectedOp) {
 		t.Error("Invalid storage operation")
 	}
 }
